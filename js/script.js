36b77fa587401f9620f03003ec8b43317f4d3a11
Ismael Pastor <ispaluan@gmail.com>
2025-03-23 20:32:15 +0100

    Avanzando en el ejercicio

--- js/script.js
+++ js/script.js
@@ -7,5 +7,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
         }return response.json();
     })  
     .then ((data) => {
-        console.log(data)
-    })
+        data.forEach(element => {
+            listaUsuarios.innerHTML += `
+            <div id="card">
+              <img src="" alt=""/>
+              <div id="datosPersonales">
+                <li><strong>Nombre:</strong>${element.name}</li>
+                <p><strong>Edad:</strong></p>
+                <p><strong>Username:</strong>${element.username}</p>
+                <p><strong>Teléfono:</strong>${element.phone}</p>
+                <p><strong>Email:</strong>${element.email}</p>
+              </div>
+              <p><strong>Compañía:</strong>${element.company.name}</p>
+              <p><strong>Dirección:</strong>${element.address.street},${element.address.suite},${element.address.city}</p>
+
+            </div>`
+
+        })
+    }) 
